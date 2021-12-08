e60486d09891ac1c4ba5f7332ab3afb3919ab1e9
Selim Saukkomaa <selim.saukkomaa@gmail.com>
2021-12-08 15:51:19 +0200

    initial commit

---
+++ public/js/chat.js
@@ -0,0 +1,47 @@
+const socket = io()
+
+// Elements
+
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $locationButton = document.querySelector('#send-location')
+
+socket.on('message', (message) => {
+    console.log(message)
+})
+
+$messageForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    $messageFormButton.setAttribute('disabled', 'disabled')
+
+    const message = e.target.elements.message.value
+
+    socket.emit('sendMessage', message, (error) => {
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
+        if (error) {
+            return console.log(error)
+        }
+        console.log('Message delivered!')
+    })
+})
+
+$locationButton.addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser.')
+    }
+    $locationButton.setAttribute('disabled', 'disabled')
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {
+            lat: position.coords.latitude,
+            long: position.coords.longitude
+        }, (message) => {
+            $locationButton.removeAttribute('disabled')
+            console.log(message)
+        })
+        
+    })
+})
